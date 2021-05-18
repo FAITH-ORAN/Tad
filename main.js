6da82980e76f1a6e2abe4194284bbb227fa1848a
Faiza <berrichif0717@gmail.com>
2021-05-18 19:46:54 +0200

    ajout des exo condition 1 et 2

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var age=prompt("votre age svp: ");
-
-switch (age.toString()) {
-    case "18": 
-    alert("ous êtes majeur");
-    break
+//exo 1 condition
+var a =10;
+var b= 5;
+if(a>b){
+    alert("le nombre max est : " + a);
+}else{
+    alert("le nombre max est :" + b);
 }
+
+//exo 2 condition
+function getTheNumber(){
+var val=document.getElementById("num").value;
+if(val%2 == 0){//&&(typeof(val==Number))
+    alert(val+" est un nombre pair")}
+else{
+    alert(val+" est un nombre impaire");
+}};
+
